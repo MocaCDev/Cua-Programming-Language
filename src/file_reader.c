555c33ee8343ad3963eb545b7114dcbe295b34f9
GetStuffDone <t.targetyt@gmail.com>
2020-08-24 04:10:08 +0000

    file extension .cua is a must

--- src/file_reader.c
+++ src/file_reader.c
@@ -7,18 +7,19 @@ char* read_file(const char* file_to_read) {
     char* buffer = 0;
     long length;
 
-    int index = 0;
+    int index = 0, length_ = 0;
     char* extension = calloc(1,sizeof(char));
 
     for(;index < strlen(file_to_read); index++) {
         if(file_to_read[index] == '.') {
             while(file_to_read[index] != '\0') {
-                char* value = calloc(1,sizeof(char));
+                char* value = calloc(1,sizeof(char*));
 
                 value[0] = file_to_read[index];
                 value[1] = '\0';
 
-                extension = realloc(extension,strlen(value));
+                length_++;
+                extension = realloc(extension,length_*sizeof(char*));
                 strcat(extension,value);
                 index++;
             }
